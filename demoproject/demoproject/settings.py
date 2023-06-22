9b12be541275e86441da52f6bf7a0a57ff029538
Akhilesh <akhilesh.soni@somaiya.edu>
2023-06-22 14:49:21 +0530

    vercel done

--- demoproject/demoproject/settings.py
+++ demoproject/demoproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=68f!$&52q(a=4&#cj+qvq^e4v7t3!5)(xr2ep1yth&6v-5e!i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
